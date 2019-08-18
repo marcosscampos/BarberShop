66fa80a65adf7050e03ac2ed8d6095c1de38165b
Marcos Vinicius <marcosvinicius.analucia@gmail.com>
2019-08-17 23:45:46 -0300

    First Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# BarberShop
+<h1>BarberShop</h1>
+<h3>Sua barbearia virtual</h3>
---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
+
+class App {
+    constructor() {
+        this.express = express()
+        this.idDev = process.env.NODE_ENV !== 'production'
+        this.middlewares()
+        this.Views()
+        this.routes()
+    }
+    middlewares() {
+        this.express.use(express.urlencoded({ extended: false }))
+    }
+    Views() {
+        nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+            watch: this.idDev,
+            express: this.express,
+            autoescape: true
+        })
+        this.express.set('view engine', 'njk')
+    }
+    routes() {
+
+    }
+}
+module.exports = new App().express
